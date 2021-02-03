947e319971ba873ac0dbf692e32aff60cffb5d92
arthurlpgc <arthurlpgcosta@gmail.com>
2021-02-03 17:15:10 +0000

    tele bot

--- server.py
+++ server.py
@@ -1,31 +1,30 @@
-import os, time, json
-from chessdotcom import get_player_stats 
+import os
+import time
+import json
 import discord
+from chessdotcom import get_player_stats
+from rating import get_ratings
+
 
 TOKEN = json.loads(open('secret.json', 'r').read())["discord_token"]
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+
 @client.event
 async def on_message(message):
     if message.content.startswith('!chessrating '):
         player = message.content.split(' ')[1]
-        stats = get_player_stats(player).json
+        ratings = get_ratings(player)
         reply = f'Ratings for {player}:\n'
-        for chess_type in stats:
-            if type(stats[chess_type]) is not dict:
-                continue 
-            if 'last' not in stats[chess_type]:
-                continue
-            if 'rating' not in stats[chess_type]['last']:
-                continue
-            rating = stats[chess_type]['last']['rating']
-            rd = None
-            if 'rd' in stats[chess_type]['last']:
-                rd = stats[chess_type]['last']['rd']
+        for chess_type in ratings:
+            stat = ratings[chess_type]
+            rating = stat['rating']
+            rd = stat['rd']
             reply += f'  {chess_type}: {rating} RD({rd})\n'
         await message.channel.send(reply)
 
--- telebot.py
+++ telebot.py
@@ -1,3 +1,34 @@
-from telegram.ext import Updater
-updater = Updater(token='TOKEN', use_context=True)
+from telegram.ext import Updater, CommandHandler
+import json
+from rating import get_ratings
+
+TOKEN = json.loads(open('secret.json', 'r').read())["telegram_bot"]
+
+updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
+
+
+def get_rating(update, context):
+    words = update.message.text.split(' ')
+    if len(words) == 1:
+        context.bot.send_message(
+            chat_id=update.effective_chat.id, text="Please send the username as well!")
+    elif len(words) > 2:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="What are you trying to pull here? Send just one username")
+    else:
+        player = words[1]
+        ratings = get_ratings(player)
+        message = f'Ratings for {player}\n'
+        for (chess_type, stats) in ratings.items():
+            rating = stats['rating']
+            rd = stats['rd']
+            mode = chess_type.split('chess_')[1]
+            message += f'{mode}:  {rating} RD({rd})\n'
+        context.bot.send_message(
+            chat_id=update.effective_chat.id, text=message)
+
+
+start_handler = CommandHandler('rating', get_rating)
+dispatcher.add_handler(start_handler)
+updater.start_polling()
